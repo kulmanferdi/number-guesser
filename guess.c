5bee4ef9aecc2e6f1ec50a615f50856c00ea0a30
kulmanferdi <93187610+kulmanferdi@users.noreply.github.com>
2022-11-10 19:24:54 +0100

    version 1.1
    -minor optimizations
    -implemented ENUM
    -updated performance rating Strings
    
    2022.11.10

--- guess.c
+++ guess.c
@@ -3,40 +3,36 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <ctype.h>
-#define MIN_GUESS 1
-#define MAX_GUESS 10
 #define LEGENDARY 1
-#define GREAT MAX_GUESS*0.3
-#define GOOD MAX_GUESS*0.5
-#define POOR MAX_GUESS*0.7
-#define CURRENT_VERSION "1.0.1"
+#define GREAT MAX*0.3
+#define GOOD MAX*0.5
+#define POOR MAX*0.7
+#define CURRENT_VERSION "1.1"
+enum GUESSRANGE {MIN = 1, MAX = 10};
 typedef float real;
 int main(void)
 {
-    int8_t playAgain, title [] = {"Number Guesser Game"}, roundString [] = {"Round"};
+    int8_t playAgain, title [] = {"Number Guesser Game"};
     int16_t guess, tries, number, round=0;
     real sumGuess=0;
     do {
         ++round;
         system("clear");
-        printf("%s\n%s: %d\n\n",title,roundString,round);
-        printf("Guess a number between %d and %d\n",MIN_GUESS,MAX_GUESS);
+        printf("%s\n%d. round\n\n",title,round);
+        printf("Guess a number between %d and %d\n",MIN,MAX);
         srand(time(NULL));
-        number = rand() % MAX_GUESS + 1;
+        number = rand() % MAX + 1;
         tries = 0;
         do {
             do {
                 printf("%d. guess: ",tries+1);
                 scanf("%hd", &guess);
-            } while (guess < MIN_GUESS | guess > MAX_GUESS);
+            } while (guess < MIN | guess > MAX);
             ++tries;
-            ++sumGuess;
-            number == guess ?
-                printf("Correct! It is %d!\nNumber of tries: %d\nAverage number of guesses: %.1f\n",number,tries,sumGuess/round) :
-                number > guess ?
-                    printf("Bigger.\n") :
-                    printf("Smaller.\n");
-        } while (guess != number);
+            ++sumGuess;       
+            number > guess ? printf("Bigger.\n") : number < guess ? printf("Smaller.\n") : 
+            printf("Correct! It is %d!\nNumber of tries: %d\nAverage number of guesses: %.1f\n",number,tries,sumGuess/round);
+        } while (guess != number);       
         do{            
             printf("Do you want to play again? [y/n]: ");
             getchar();
@@ -44,12 +40,12 @@ int main(void)
         } while (toupper(playAgain) != 'N' && toupper(playAgain) != 'Y');
     } while (toupper(playAgain) == 'Y');
     system("clear");
-    printf("%s\n\n%ss played: %d\nYour average: %.1f\nPerformance rating: ",title,roundString,round,sumGuess/round);
-    if(sumGuess/round == LEGENDARY) printf("YOU ARE AMAZING. You can't miss.\n");
-    else if(sumGuess/round <= GREAT) printf("Decent game. Well played.\n"); 
-    else if(sumGuess/round <= GOOD) printf("Not that bad, nor great either.\n");
-    else if(sumGuess/round <= POOR) printf("You could have done better, but still managed to guess them all.\n");
-    else printf("You better take a long break.\n");
+    printf("%s\n\nRounds played: %d\nYour average: %.1f\nPerformance rating: ",title,round,sumGuess/round);
+    if(sumGuess/round == LEGENDARY) printf("YOU. ARE. GODLIKE.\n");
+    else if(sumGuess/round <= GREAT) printf("Decent game.\n"); 
+    else if(sumGuess/round <= GOOD) printf("This was good.\n");
+    else if(sumGuess/round <= POOR) printf("It was not good at all, not gonna lie.\n");
+    else printf("You better take a very long break.\n");
     printf("\nThanks for playing!\nGame version: %s\n",CURRENT_VERSION);
     return 0;
 }
